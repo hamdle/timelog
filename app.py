aa9240e529dd15fcaa14e80003428c12128325a5
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-07-28 17:06:12 -0500

    Create items and display in GUI

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from time import strftime
+from tkinter.ttk import Style
+from PIL import Image, ImageTk
 
-root = Tk()
-root.geometry('350x500')
-root.resizable(False, False)
-root.title('Timelog')
 
 def time():
     system_time = strftime('%H:%M:%S')
-    label.config(text=system_time)
-    label.after(1000, time)
+    time_label.config(text=system_time)
+    time_label.after(1000, time)
 
-label = Label(root, font=('calibri', 40, 'bold'))
-label.pack(anchor='center')
+def start_button_press():
+    print(start_button['text'])
+    start_button['text'] = 'Start' if start_button['text'] == 'Stop' else 'Stop'
+    save_button['state'] = tk.NORMAL
+
+def save_button_press():
+    save_button['state'] = tk.DISABLED
+    print("SAVE")
+
+root = tk.Tk()
+root.geometry('340x490')
+root.resizable(False, False)
+root.title('Timelog')
+root.configure(background='white')
+
+frame_style = Style()
+frame_style.configure('timelog.TFrame', background='white')
+
+frame = ttk.Frame(root, padding=10, style='timelog.TFrame')
+frame.grid()
+frame.pack(anchor='center')
+
+logo = Image.open('logo.png')
+logo = logo.resize((50,50))
+logo_photo = ImageTk.PhotoImage(logo)
+logo_label = ttk.Label(frame, image=logo_photo)
+logo_label.grid(column=0, row=0, pady=(60,0))
+
+time_label = ttk.Label(frame, font=('calibri', 45), background='white')
+time_label.grid(column=0, row=1, pady=(60,0))
+
+start_button = ttk.Button(frame, text='Start', command=start_button_press, width=18)
+start_button.grid(column=0, row=2, pady=(15,0))
+
+save_button = ttk.Button(frame, text='Save', command=save_button_press)
+save_button.grid(column=0, row=3, pady=(70,0))
+save_button['state'] = tk.DISABLED
 
 time()
- 
+
 root.mainloop()
--- readme.txt
+++ readme.txt
@@ -5,3 +5,5 @@ Python GUI application to log time to a local file.
 Install
 -------
 $ sudo dnf install python3-tkinter
+$ sudo dnf install python3-pillow-tk
+
